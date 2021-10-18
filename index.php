f79c00a2f59fdb30c6e33c0186c03cb16dac8862
Annisa Artha <annisa.artha@gmail.com>
2021-10-18 21:42:09 +0700

    edit 3

--- index.php
+++ index.php
@@ -28,33 +28,46 @@
 <br>
 
 <h1>Search User Data</h1>
-<form action="index.php" method="POST">
-	<input type="text" name="search_user" placeholder="Enter username" required>
-	<input type="submit" value="search">
+<form action="" method="POST">
+	<input type="text" name="get_user" placeholder="Enter username" required>
+	<input type="submit" name="search_user" value="search">
 </form>
+
+<?php
+	if(isset($_POST['search_user'])){
+		$search = $_POST['get_user'];
+		$query = "SELECT * FROM users WHERE user_uid='$search';";
+		$query_run = mysqli_query($conn, $query);
+?>
 
 <br>
 <table border="1">
 	<tr>
-		<td>User ID</td>
+		<td>Username</td>
 		<td>First Name</td>
 		<td>Last Name</td>
 		<td>Email</td>
-		<td>Username</td>
 	</tr>
+	<?php
+		if(mysqli_num_rows($query_run) > 0){
+			while($row = mysqli_fetch_array($query_run)){
+	?>
+	<tr>
+		<td><?php echo $row['user_uid']; ?></td>
+		<td><?php echo $row['user_first']; ?></td>
+		<td><?php echo $row['user_last']; ?></td>
+		<td><?php echo $row['user_email']; ?></td>
+	</tr>
+	<?php
+		}
+	}else {
+		echo "No Data Found";
+	}
+	?>
 </table>
-
 <?php
-	$sql = "SELECT * FROM users;";
-    $result = mysqli_query($conn, $sql);
-    $resultCheck = mysqli_num_rows($result);
-
-    if ($resultCheck > 0){
-        while ($row = mysqli_fetch_assoc($result)){
-            echo $row['user_uid'] . "<br>";
-        }
-    }
-?>
+	}
+	?>
 
 </body>
 </html>
